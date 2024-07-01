2b3b6860e6a9e139f7acc07770bc6049dfb1d38a
MeenS4 <rokoszoczek@gmail.com>
2024-07-01 13:46:00 +0200

    feat: update product-card component & home-page styling

---
+++ src/pages/home/home-page.tsx
@@ -0,0 +1,19 @@
+import { ProductCard } from "../../components";
+import { Navbar } from "../../sections";
+import styles from "./home-page.module.scss";
+
+export const HomePage = () => {
+  return (
+    <main className={styles["home-page"]}>
+      <Navbar />
+
+      <div className={styles["home-page__products"]}>
+        <ProductCard title="Hoodie" price={79.99} />
+
+        <ProductCard title="T-shirt" price={19.99} />
+
+        <ProductCard title="Sneakers" price={299.98} />
+      </div>
+    </main>
+  );
+};
