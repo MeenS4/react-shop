e2b24dc968d6ebf69a2c5a61be87370f0bfab53c
MeenS4 <rokoszoczek@gmail.com>
2024-07-01 13:43:56 +0200

    feat: add basic page routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./app";
 import "./index.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage } from "./pages/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/cart",
+    element: <h1>Your cart!</h1>,
+  },
+]);
 
 const entryPoint = document.querySelector("#root");
 
-ReactDOM.createRoot(entryPoint as HTMLElement).render(<App />);
+ReactDOM.createRoot(entryPoint as HTMLElement).render(
+  <RouterProvider router={router} />,
+);
