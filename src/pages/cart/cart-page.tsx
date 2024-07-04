b1b163f65540632de28419b48e8e86991c5fe44c
MeenS4 <rokoszoczek@gmail.com>
2024-07-04 14:13:59 +0200

    feat: display added items in CartPage

--- src/pages/cart/cart-page.tsx
+++ src/pages/cart/cart-page.tsx
@@ -1,6 +1,10 @@
-import { useSelector } from "react-redux";
 import styles from "./cart-page.module.scss";
+
+import { useSelector } from "react-redux";
+
 import { selectCartItems } from "../../store/cartSlice";
+import { Product } from "../../store/productSlice";
+import { ProductCard } from "../../components";
 
 export const CartPage = () => {
   const cartItems = useSelector(selectCartItems);
@@ -8,7 +12,16 @@ export const CartPage = () => {
   return (
     <main className={styles["cart-page"]}>
       <h1>My Cart</h1>
-      {cartItems}
+
+      <div className={styles["cart-page__items"]}>
+        {cartItems.map((product: Product) => (
+          <ProductCard
+            id={product.id}
+            title={product.title}
+            price={product.price}
+          />
+        ))}
+      </div>
     </main>
   );
 };
